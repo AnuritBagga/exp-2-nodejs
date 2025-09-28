203ab35d65592b5d756d482eafe148e85687cc7e
AnuritBagga <166058029+AnuritBagga@users.noreply.github.com>
2025-09-28 16:40:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,11 @@ import cardRoutes from './routes/index.js';
 
 const app = express();
 const PORT = 3000;
-
-// Middleware to parse JSON
 app.use(bodyParser.json());
-
-// API routes
 app.use('/api', cardRoutes);
-
-// Root route
 app.get('/', (req, res) => {
   res.send('Welcome to the Playing Card Collection API!');
 });
-
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
